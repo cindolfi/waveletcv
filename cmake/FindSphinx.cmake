fec39300f41c80352fc5299147973736c377749d
Chris <cmindolfi@gmail.com>
2024-07-12 14:10:09 -0400

    reworked docs build for read the docs

--- cmake/FindSphinx.cmake
+++ cmake/FindSphinx.cmake
@@ -1,4 +1,4 @@
-#Look for an executable called sphinx-build
+#   Look for an executable called sphinx-build
 find_program(
     SPHINX_EXECUTABLE
     NAMES sphinx-build
@@ -7,9 +7,16 @@ find_program(
 
 include(FindPackageHandleStandardArgs)
 
-#Handle standard arguments to find_package like REQUIRED and QUIET
+#   Handle standard arguments to find_package like REQUIRED and QUIET
 find_package_handle_standard_args(
     Sphinx
     "Failed to find sphinx-build executable"
     SPHINX_EXECUTABLE
 )
+
+#   Get the version
+execute_process(
+    COMMAND ${SPHINX_EXECUTABLE} --version
+    OUTPUT_VARIABLE SPHINX_VERSION
+)
+string(REGEX MATCH "[0-9.]+" SPHINX_VERSION "${SPHINX_VERSION}")
